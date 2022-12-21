d6c1be6093e1e09f7bfaf011ecdbf9713c2d0d15
Caio <florianita100.mendes@gmail.com>
2022-12-21 18:55:53 -0300

    Fix semicolon in World file

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -8,7 +8,7 @@ public class World : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Generate()
+        Generate();
     }
 
     // Update is called once per frame
